2aac198ce50b3732b8dbc26ea4b1fff83896f016
hector paz <hectordpazs@gmail.com>
2021-12-11 16:03:11 -0500

    all app here

---
+++ docs/precache-manifest.5c83d4a0518eaa88096628dbc6f01b71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e6a1462343eb44084edb48c453752d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e936b32e57fa269a967",
+    "url": "/static/css/main.9eb48dc4.chunk.css"
+  },
+  {
+    "revision": "af2cc4927ee53754f4c8",
+    "url": "/static/js/2.29edc68e.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.29edc68e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e936b32e57fa269a967",
+    "url": "/static/js/main.04061888.chunk.js"
+  },
+  {
+    "revision": "758b918abc6d3379bc25",
+    "url": "/static/js/runtime-main.a14c7b94.js"
+  }
+]);
